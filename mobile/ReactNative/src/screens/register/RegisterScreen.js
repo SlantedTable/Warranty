500570d3acf72cfcfaced38b469ceb045079cffc
SlantedTable <agraemelowe@gmail.com>
2019-09-16 11:39:23 -0400

    hooking up register

--- mobile/ReactNative/src/screens/register/RegisterScreen.js
+++ mobile/ReactNative/src/screens/register/RegisterScreen.js
@@ -1,10 +1,114 @@
 import React, { Component } from "react";
 
 import styles from "./style";
+import { Auth } from "aws-amplify";
 import { Button, Keyboard, Text, View, TextInput, TouchableWithoutFeedback, Alert, KeyboardAvoidingView } from 'react-native';
 
 
 export default class RegisterScreen extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          isLoading: false,
+          email: "",
+          phone: "",
+          given_name: "",
+          family_name: "",
+          password: "",
+          confirmPassword: "",
+        //   confirmationCode: "",
+          newUser: null
+        };
+    
+        this.formatPhoneNumber = this.formatPhoneNumber.bind(this);
+      }
+
+    validateForm() {
+        return (
+          this.state.email.length > 0 &&
+          this.state.phone.length > 0 &&
+          this.state.family_name.length > 0 &&
+          this.state.given_name.length > 0 &&
+          this.state.password.length > 0 &&
+          this.state.password === this.state.confirmPassword
+        );
+      }
+    
+    //   validateConfirmationForm() {
+    //     return this.state.confirmationCode.length > 0;
+    //   }
+    
+      handleChange = event => {
+        this.setState({
+          [event.target.id]: event.target.value
+        });
+      }
+
+      formatPhoneNumber(phoneNumberString) {
+        var x = phoneNumberString;
+        x = x.replace(/\D+/g, '')
+            .replace(/(\d{3})(\d{3})(\d{4})/, '+01$1$2$3');
+        return x;
+      }
+    
+      handleSubmit = async event => {
+        event.preventDefault();
+    
+        this.setState({ isLoading: true });
+    
+        try {
+          const newUser = await Auth.signUp({
+            username: this.state.email,
+            password: this.state.password,
+            attributes: {
+              phone_number: this.formatPhoneNumber(this.state.phone),   // optional - E.164 number convention
+              given_name: this.state.given_name,
+              family_name: this.state.family_name,
+            }
+          });
+          this.setState({
+            newUser
+          });
+          this.props.navigation.navigate('Home')
+        } catch (e) {
+          alert(e.message);
+        }
+    
+        this.setState({ isLoading: false });
+      }
+
+    //   handleConfirmationSubmit = async event => {
+    //     event.preventDefault();
+    
+    //     this.setState({ isLoading: true });
+    
+    //     try {
+    //       await Auth.confirmSignUp(this.state.email, this.state.confirmationCode);
+    //       await Auth.signIn(this.state.email, this.state.password);
+    
+    //       this.props.userHasAuthenticated(true);
+    //       this.props.history.push("/");
+    //     } catch (e) {
+    //       alert(e.message);
+    //       this.setState({ isLoading: false });
+    //     }
+    //   }
+
+    // componentDidMount() {
+    // }
+
+    // componentWillUnmount() {
+    // }
+
+    // onLoginPress() {
+
+    // }
+
+    // onRegisterPress() {
+
+    // }
+
     render() {
         return (
             <KeyboardAvoidingView style={styles.containerView} behavior="padding">
@@ -21,7 +125,7 @@ export default class RegisterScreen extends Component {
                             <TextInput placeholder="Reenter password" placeholderColor="#c4c3cb" style={styles.registerFormTextInput} secureTextEntry={true} />
                             <Button
                                 buttonStyle={styles.registerButton}
-                                onPress={() => this.props.navigation.navigate('Home')}
+                                onPress={this.handleSubmit}
                                 title="Register"
                             />
                             <Button
@@ -34,20 +138,6 @@ export default class RegisterScreen extends Component {
                 </TouchableWithoutFeedback>
             </KeyboardAvoidingView>
         );
-    }
-
-    componentDidMount() {
-    }
-
-    componentWillUnmount() {
-    }
-
-    onLoginPress() {
-
-    }
-
-    onRegisterPress() {
-
     }
 
 }
