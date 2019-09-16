b4def83afc67cb93dbc64a2e3f60f48ecc25507d
Henry Harris <henrythomasharris@gmail.com>
2019-09-16 12:00:48 -0400

    Boilerplate of handlers

--- api/http/handlers/warranty/create.js
+++ api/http/handlers/warranty/create.js
@@ -1,24 +1,25 @@
 // Create warranty handler
-import uuid from "uuid";
-import * as dynamoDbLib from "./libs/dynamodb-lib";
-import { success, failure } from "./libs/response-lib";
+import uuid from 'uuid'
+import * as dynamoDbLib from '../../../libs/dynamodb-lib'
+import { success, failure } from '../../../libs/response-lib'
 
 export async function main(event, context) {
-  const data = JSON.parse(event.body);
+  const data = JSON.parse(event.body || '{}')
+
   const params = {
-    TableName: "warranty",
+    TableName: 'warranty',
     Item: {
       userId: event.requestContext.identity.cognitoIdentityId,
       warrantyId: uuid.v1(),
       content: data.content,
-      createdAt: Date.now()
-    }
-  };
+      createdAt: Date.now(),
+    },
+  }
 
   try {
-    await dynamoDbLib.call("put", params);
-    return success(params.Item);
+    await dynamoDbLib.call('put', params)
+    return success(params.Item)
   } catch (e) {
-    return failure({ status: false });
+    return failure({ e, status: false })
   }
 }
