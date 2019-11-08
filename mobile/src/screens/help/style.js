a9638431646d35cffb0d81e87634b29eab821f73
Henry Harris <henrythomasharris@gmail.com>
2019-11-09 07:09:35 +0900

    ✨Add tab bar (#38)

---
+++ mobile/src/screens/help/style.js
@@ -0,0 +1,16 @@
+const React = require('react-native')
+
+const { StyleSheet } = React
+
+export default {
+  mainContainer: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  headerContainer: {
+    flex: 1,
+    alignItems: 'stretch',
+  },
+}
